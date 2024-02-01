7a5d9c7af6f475f8ceab4a89861e2454582cb010
Ryan Draves <19778373+RyanDraves@users.noreply.github.com>
2024-02-01 03:00:15 -0500

    Add a Jekyll site (#5)
    Add initial setup for a Jekyll site. It doesn't particularly work, due
    to some remaining issues with setting up Bazel rules.

---
+++ rules_jekyll/jekyll.bzl
@@ -0,0 +1,71 @@
+def _jekyll_site_impl(ctx):
+    config = ctx.attr.config
+    config_path = ctx.expand_location("$(location {target})".format(target=config.label), targets=[config])
+
+    build_files = depset(ctx.files.srcs)
+    build_destination = "{bin_dir}/{package}/{destination}".format(bin_dir=ctx.bin_dir.path, package=ctx.label.package, destination=ctx.attr.destination)
+    args = ["build", "--config", config_path, "--source", ctx.label.package, "--destination", build_destination, "--verbose"]
+
+    output_directory = ctx.actions.declare_directory(ctx.attr.destination)
+
+    # Build the site
+    ctx.actions.run(
+        inputs = build_files,
+        outputs = [output_directory],
+        arguments = args,
+        executable = ctx.executable.jekyll,
+        mnemonic = "JekyllBuild",
+        env = {
+            "LC_ALL": "C.UTF-8",
+            "LANG": "en_US.UTF-8",
+            "LANGUAGE": "en_US.UTF-8",
+        }
+    )
+
+    # Compile transitive runfiles + our build files
+    runfiles = ctx.runfiles(ctx.attr.config.files.to_list() + [output_directory])
+    transitive_runfiles = []
+    for runfiles_attr in (
+        ctx.attr.deps,
+        [ctx.attr.config, ctx.attr.jekyll],
+    ):
+        for target in runfiles_attr:
+            transitive_runfiles.append(target[DefaultInfo].default_runfiles)
+    runfiles = runfiles.merge_all(transitive_runfiles)
+
+    # This relative path works nicer for `bazel run`
+    servce_destination = ctx.label.package + "/_site"
+    args = ["serve", "--destination", servce_destination, "--skip-initial-build", "--config", config_path]
+
+    # rules_ruby needs RUNFILES_DIR to be set
+    executable = "export RUNFILES_DIR=$(readlink -f ../)\n"
+    executable += ctx.attr.jekyll.files_to_run.executable.short_path + " " + " ".join(args) + " $@\n"
+
+    ctx.actions.write(
+        output = ctx.outputs.executable,
+        is_executable = True,
+        content = executable)
+
+    # Collect file outputs for any dependents
+    serve_files = depset(ctx.attr.config.files.to_list() + [output_directory])
+
+    return [
+        DefaultInfo(
+            executable = ctx.outputs.executable,
+            files = serve_files,
+            runfiles = runfiles,
+        ),
+    ]
+
+
+jekyll_site = rule(
+    implementation = _jekyll_site_impl,
+    executable = True,
+    attrs = {
+        "srcs": attr.label_list(allow_files = True),
+        "deps": attr.label_list(),
+        "destination": attr.string(default = "_site"),
+        "config": attr.label(allow_single_file = True, mandatory = True),
+        "jekyll": attr.label(executable = True, cfg="exec", mandatory = True),
+    },
+)
--- setup.sh
+++ setup.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
+#
+# This script sets up a new Ubuntu 22.04 LTS installation with the following:
+# - apt packages
+# - bazelisk
+# - udev rules
+# - Ryan's custom settings (optional)
+#     - gh
+#     - vscode extensions
+#     - vscode keybindings
+#     - gh alias
+#
+# It probably works on anything Linux x86_64, but YMMV.
+#
+# Usage:
+#     ./setup.sh
 
 set -e
+set -o pipefail
 
 BAZELISK_VERSION=v1.19.0
 BAZELISK_SHA384_SUM=cc454c248a90c414bb1774e798ba87ecea6e81127f06c2ba73e93dab51a9159ca67a65deb2cc0b38095314e6a2a7a667
+
+GH_VERSION="2.42.0"
+GH_SHA384_SUM=c6d8060affe74fea9473d1c2393e5dc4e858521ebb6eb39db5551deaf442158e5f82edfb9d3680c5c14d72513601bf75
+
+REPO_ROOT=$(dirname $(readlink -f $0))
 
 APT_PACKAGES=(
     build-essential
@@ -11,19 +32,54 @@ APT_PACKAGES=(
     git
     unzip
     wget
+    default-jdk
     zip
+    zlib1g-dev
     libusb-1.0-0-dev
+    tree
+    htop
     # For local Pico tooling
     pkg-config
     cmake
-    gcc-arm-none-eabi
-    libnewlib-arm-none-eabi
-    libstdc++-arm-none-eabi-newlib
     # For local Pico testing
     minicom
 )
 
+function check_if_on_wsl() {
+    if uname -r | grep -q "WSL"; then
+        return 0
+    fi
+    return 1
+}
+
+RYANS_APT_PACKAGES=()
+if ! check_if_on_wsl; then
+    RYANS_APT_PACKAGES+=(
+        code
+    )
+fi
+
+RYANS_VSCODE_EXTENSIONS=(
+    bazelbuild.vscode-bazel
+    bpruitt-goddard.mermaid-markdown-syntax-highlighting
+    github.copilot
+    github.copilot-chat
+    github.vscode-github-actions
+    jeff-hykin.better-cpp-syntax
+    ms-python.python
+    ms-python.vscode-pylance
+    ms-vscode.cpptools
+    ms-vscode.cpptools-themes
+)
+if ! check_if_on_wsl; then
+    RYANS_VSCODE_EXTENSIONS+=(
+        ms-vscode-remote.remote-ssh
+    )
+fi
+
 function install_apt_packages() {
+    local packages=("$@")
+
     # Check if apt is available
     if ! check_command apt; then
         echo "apt is not available"
@@ -33,12 +89,12 @@ function install_apt_packages() {
     # Check if apt packages are already installed
     all_installed=true
     missing_packages=()
-    for package in ${APT_PACKAGES[@]}; do
-        if dpkg -s $package &> /dev/null; then
+    for package in "${packages[@]}"; do
+        if dpkg -s "$package" &> /dev/null; then
             continue
         fi
         all_installed=false
-        missing_packages+=($package)
+        missing_packages+=("$package")
     done
 
     if $all_installed; then
@@ -50,17 +106,16 @@ function install_apt_packages() {
 
     # Install apt packages
     sudo apt update
-    sudo apt install -y ${missing_packages[@]}
+    sudo apt install -y "${missing_packages[@]}"
 }
 
 function filesystem_setup() {
     add_to_path $HOME/.local/bin
 
     mkdir -p $HOME/.local/share/completions
-    # Add ~/.local/share/completions to ~/.bashrc if not already present
-    if ! grep -q "source ~/.local/share/completions/*" ~/.bashrc; then
-        echo "source ~/.local/share/completions/*" >> ~/.bashrc
-    fi
+    maybe_add_to_bashrc "source $HOME/.local/share/completions/*"
+
+    maybe_add_to_bashrc "export JAVA_HOME=/usr/lib/jvm/default-java"
 }
 
 function install_bazelisk() {
@@ -110,6 +165,140 @@ function copy_udev_rules() {
     sudo udevadm control --reload-rules
 }
 
+#
+# Ryan's custom settings
+#
+
+function setup_ryans_custom_settings() {
+    setup_gh
+    install_apt_packages "${RYANS_APT_PACKAGES[@]}"
+    install_vscode_extensions "${RYANS_VSCODE_EXTENSIONS[@]}"
+    install_vscode_keybindings misc/dravesr/keybindings.json
+}
+
+function setup_gh() {
+    install_gh
+    authenticate_gh
+    make_gh_alias feature 'issue develop "$1" -c'
+}
+
+function install_gh() {
+    # Check if gh is already installed and has the correct version
+    if [[ "$(gh --version)" == *"$GH_VERSION"* ]]; then
+        echo "gh is already installed"
+        return 0
+    fi
+
+    # Install gh from release
+    echo "Installing gh v$GH_VERSION"
+    local gh_url="https://github.com/cli/cli/releases/download/v$GH_VERSION/gh_${GH_VERSION}_linux_amd64.tar.gz"
+
+    wget $gh_url -O /tmp/gh.tar.gz
+    verify_sha384sum /tmp/gh.tar.gz $GH_SHA384_SUM
+
+    # Unpack gh.tar.gz to ~/.local
+    tar -xzf /tmp/gh.tar.gz -C ~/.local --strip-components=1
+
+    # Verify is installed with the correct version
+    version=$(gh --version)
+    if ! echo $version | grep -q $GH_VERSION; then
+        echo -e "\e[31mgh is not installed correctly"
+        echo "mExpected version: $GH_VERSION"
+        echo -e "Actual version: $version\e[0m"
+        exit 1
+    fi
+}
+
+function authenticate_gh() {
+    # Check if gh is already authenticated
+    if gh auth status | grep -q "Logged in to github.com"; then
+        echo "gh is already authenticated"
+        return 0
+    fi
+
+    # Authenticate gh if the caller is in an interactive shell
+    if [[ "$-" == *i* ]]; then
+        echo "Authenticating gh"
+        gh auth login -p ssh -w
+    else
+        echo "gh is not authenticated and the caller is not in an interactive shell"
+        return 1
+    fi
+}
+
+function make_gh_alias() {
+    local alias=$1
+    local command=$2
+    # Check if a matching alias and command already exist
+    if gh alias list | grep -q "$alias"; then
+        if gh alias list | grep -qx "$alias: $command"; then
+            echo "gh alias $alias already exists"
+            return 0
+        fi
+        echo "gh alias $alias already exists with a different command"
+        gh alias set $alias "$command"
+    else
+        echo "Creating gh alias $alias"
+        gh alias set $alias "$command"
+    fi
+}
+
+function install_vscode_extensions() {
+    local extensions=("$@")
+
+    # Check if code is available
+    if ! check_command code; then
+        echo "code is not available"
+        return 1
+    fi
+
+    # Check if vscode extensions are already installed
+    all_installed=true
+    missing_extensions=()
+    for extension in "${extensions[@]}"; do
+        if code --list-extensions | grep -iq "$extension"; then
+            continue
+        fi
+        all_installed=false
+        missing_extensions+=("$extension")
+    done
+
+    if $all_installed; then
+        echo "All vscode extensions are already installed"
+    else
+        echo "Installing missing vscode extensions: ${missing_extensions[@]}"
+
+        # Install vscode extensions
+        for extension in "${missing_extensions[@]}"; do
+            code --install-extension $extension
+        done
+    fi
+
+    if check_if_on_wsl; then
+        # Install ms-vscode-remote.remote-wsl on the Windows side if not already installed
+        if ! powershell.exe 'code --list-extensions' | grep -iq 'ms-vscode-remote.remote-wsl'; then
+            echo "Installing ms-vscode-remote.remote-wsl on Windows"
+            powershell.exe 'code --install-extension ms-vscode-remote.remote-wsl'
+        else
+            echo "ms-vscode-remote.remote-wsl is already installed on Windows"
+        fi
+    fi
+}
+
+function install_vscode_keybindings() {
+    local repo_keybindings_file=$1
+
+    if check_if_on_wsl; then
+        windows_user=$(powershell.exe '$env:UserName' | tr -d '\r')
+        keybindings_file=/mnt/c/Users/$windows_user/AppData/Roaming/Code/User/keybindings.json
+    else
+        keybindings_file=$HOME/.config/Code/User/keybindings.json
+    fi
+
+    if copy_if_not_up_to_date $repo_keybindings_file $keybindings_file false; then
+        echo "Copied $repo_keybindings_file to $keybindings_file"
+    fi
+}
 
 #
 # Helpers
@@ -118,11 +307,8 @@ function copy_udev_rules() {
 function add_to_path() {
     local path=$1
     mkdir -p $path
-    # Add $path to PATH in ~/.bashrc if not already present,
-    # then export the new PATH
-    if ! grep -q "PATH=\"$path:\$PATH\"" ~/.bashrc; then
-        echo "PATH=\"$path:\$PATH\"" >> ~/.bashrc
-    fi
+    maybe_add_to_bashrc "PATH=\"$path:\$PATH\""
+    # Export to make the new PATH available in the current shell
     export PATH="$path:$PATH"
 }
 
@@ -146,6 +332,13 @@ function check_command() {
     fi
 
     return 0
+}
+
+function maybe_add_to_bashrc() {
+    local line=$1
+    if ! grep -q "$line" ~/.bashrc; then
+        echo "$line" >> ~/.bashrc
+    fi
 }
 
 function success() {
@@ -174,8 +367,12 @@ function copy_if_not_up_to_date() {
     return 1
 }
 
-install_apt_packages
+install_apt_packages "${APT_PACKAGES[@]}"
 filesystem_setup
 install_bazelisk
 copy_udev_rules
+# Check if user is `dravesr` before setting up Ryan's environment
+if [ "$USER" = "dravesr" ]; then
+    setup_ryans_custom_settings
+fi
 success
