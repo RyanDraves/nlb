15d9d468031972bc384edb12c9c1f7f90ad0e92d
Ryan Draves <dravesr@umich.edu>
2024-09-10 22:56:20 -0600

    mark _bh.pyi files as generated

--- emb/network/serialize/bh_cobs_test.cc
+++ emb/network/serialize/bh_cobs_test.cc
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <span>
 #include <vector>
 
@@ -24,39 +23,11 @@ TEST(BhCobsTest, TestBasic) {
 
     auto [serialized, frame_padding] = serializer.serialize(in_data, buffer);
 
-    // Print the buffer
-    std::cout << "Serialized: ";
-    for (size_t i = 0; i < serialized.size(); i++) {
-        std::cout << std::hex << (int)serialized[i] << " ";
-    }
-    std::cout << std::endl;
-
     auto framed = serializer.frame(serialized);
-
-    // Print the buffer
-    std::cout << "Framed: ";
-    for (size_t i = 0; i < framed.size(); i++) {
-        std::cout << std::hex << (int)framed[i] << " ";
-    }
-    std::cout << std::endl;
 
     auto deframed = serializer.deframe(framed);
 
-    // Print the buffer
-    std::cout << "Deframed: ";
-    for (size_t i = 0; i < deframed.size(); i++) {
-        std::cout << std::hex << (int)deframed[i] << " ";
-    }
-    std::cout << std::endl;
-
     testdata::Foo out_data = serializer.deserialize<testdata::Foo>(deframed);
-
-    // Print out_data
-    std::cout << "Deserialized: " << out_data.bar << " " << out_data.baz << " ";
-    for (size_t i = 0; i < out_data.qux.size(); i++) {
-        std::cout << out_data.qux[i] << " ";
-    }
-    std::cout << std::endl;
 
     ASSERT_THAT(out_data.bar, Eq(in_data.bar));
     ASSERT_THAT(out_data.baz, Eq(in_data.baz));
