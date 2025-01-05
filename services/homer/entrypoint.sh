d6a0c7b9f16af182ef5955d974a0a67412de60e3
Ryan Draves <19778373+RyanDraves@users.noreply.github.com>
2025-01-05 18:06:45 -0500

    Setup self hosted services (#76)
    Tailscale's pretty cool. Sprinkle in nginx & some simple Docker compose
    setup, and I can stand up some self-host services accessible from my
    Tailnet with a reasonable URL to access things.
    
    Most everything is just infrastructure, but a "real" service is running
    via [LobeChat](https://github.com/lobehub/lobe-chat), which provides a
    way for me to use some OpenAI credits though a chat interface when the
    free stuff hits rate limits.
    
    Fun fact: your Tailnet name is randomly generated, but you are allowed
    to click `Reroll` until you're happy with it. After a few rolls I hit
    `barn-arcturus`... maybe I'll use those credits to make a logo of a bear
    wearing a farmer's hat.
    
    
    ![image](https://github.com/user-attachments/assets/44ba32e1-cbfc-4cab-8790-9cbc80eab6b9)

---
+++ services/homer/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+mkdir -p /www/assets
+mkdir -p /www/assets/icons
+cp /home/lighttpd/config.yaml /www/assets/config.yml
+cp /home/lighttpd/favicon.ico /www/assets/icons/favicon.ico
+token=$(cat /run/secrets/portainer_token)
+sed -i 's@PORTAINER_TOKEN@'"$token"'@g' /www/assets/config.yml
+
+# Rest of ntrypoint copied from https://github.com/bastienwirtz/homer
+
+# Default assets & example configuration installation
+if [[ "${INIT_ASSETS}" == "1" ]] && [[ ! -f "/www/assets/config.yml" ]]; then
+    echo "No configuration found, installing default config & assets"
+    if [[ -w "/www/assets/" ]];
+    then
+        while true; do echo n; done | cp -Ri /www/default-assets/* /www/assets/
+        yes n | cp -i /www/default-assets/config.yml.dist /www/assets/config.yml
+    else
+        echo "Assets directory not writable, skipping default config install.";
+        echo "Check assets directory permissions & docker user or skip default assets install by setting the INIT_ASSETS env var to 0."
+    fi
+fi
+
+echo "Starting webserver"
+exec lighttpd -D -f /lighttpd.conf
