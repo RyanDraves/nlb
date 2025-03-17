72922a861c08c64cc23d8d73d1e041236e10cd35
Ryan Draves <dravesr@umich.edu>
2025-03-17 13:27:18 -0600

    final fixes

--- services/mealie/down.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# See `up.sh` for explanation
-
-docker compose -p services down
--- services/mealie/up.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Add the `services` prefix so we don't have to deal with
-# moving the prod data around. I tried copying the data with
-# `    docker run --rm -v source_volume:/from -v destination_volume:/to alpine sh -c "cp -av /from/* /to"`
-# but the user auth didn't like that (different salt?).
-
-docker compose -p services up -d
