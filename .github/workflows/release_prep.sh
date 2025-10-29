3892d2673a72ea761aa164e95372baa01fe57a84
Ryan Draves <dravesr@umich.edu>
2025-10-29 12:02:07 -0600

    Fix inconsistent naming in release process

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -11,8 +11,8 @@ TAG=$1
 # The prefix is chosen to match what GitHub generates for source archives
 # This guarantees that users can easily switch from a released artifact to a source archive
 # with minimal differences in their code (e.g. strip_prefix remains the same)
-PREFIX="rules_nlb-${TAG:1}"
-ARCHIVE="rules_nlb-$TAG.tar.gz"
+PREFIX="nlb-${TAG:1}"
+ARCHIVE="nlb-$TAG.tar.gz"
 
 # NB: configuration for 'git archive' is in /.gitattributes
 git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
